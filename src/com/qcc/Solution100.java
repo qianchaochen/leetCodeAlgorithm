ad0c4b40f116354745738f629acb8b36fa9140ca
qianchaochen <qianchaochen@126.com>
2019-06-16 22:11:10 +0800

    100. 相同的树

---
+++ src/com/qcc/Solution100.java
@@ -0,0 +1,27 @@
+package com.qcc;
+
+/**
+ * @Description create by qianchaochen at 2019年06月16日22:06
+ */
+public class Solution100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        //如果两棵树都为空,则返回true;
+        if (p == null && q == null) {
+            return true;
+        }
+        //如果两棵树的根节点都不为空,且相同,则比较其对应的左右子树是否相同;
+        if (p != null && q != null && p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
