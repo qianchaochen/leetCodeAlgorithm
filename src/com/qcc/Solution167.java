487c49a352fe51da3c4fd8c975fc258a99dca9be
qianchaochen <qianchaochen@126.com>
2019-10-05 21:41:34 +0800

    优化执行时间:167.两数之和 II - 输入有序数组---改进

--- src/com/qcc/Solution167.java
+++ src/com/qcc/Solution167.java
@@ -37,4 +37,28 @@ public class Solution167 {
         }
         return result;
     }
+
+
+    /**
+     * 数组有序,使用两根指针从首尾同时开始循环.如果和大于target,则尾指针向前移;
+     * 如果和小于target,则头指针向后移.
+     * @param numbers
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] numbers, int target) {
+        int[] result = new int[2];
+        for (int i = 0, j=numbers.length-1; i < j; ) {
+            if (numbers[i] + numbers[j] == target) {
+                result[0] = i+1;
+                result[1] = j+1;
+                break;
+            }else if(numbers[i] + numbers[j] < target){
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return result;
+    }
 }
