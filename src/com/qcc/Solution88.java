3ffff5d1bf1a2afea579a524ad26e187006ee8ff
qianchaochen <qianchaochen@126.com>
2019-10-05 19:09:43 +0800

    88.合并两个有序数组

---
+++ src/com/qcc/Solution88.java
@@ -0,0 +1,42 @@
+package com.qcc;
+
+import java.util.Arrays;
+
+/**
+ * @Description 合并两个有序数组
+ * 给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+ * 说明:
+ * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+ * 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+ * 示例:
+ * <p>
+ * 输入:
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
+ * <p>
+ * 输出: [1,2,2,3,5,6]
+ * @Author：create by qianchaochen at 2019年10月05日17:28
+ */
+public class Solution88 {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        merge(nums1, 3, nums2, 3);
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] >= nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+
+    }
+}
