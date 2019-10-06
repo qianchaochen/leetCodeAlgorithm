3e2998e71d9a4fe55f1cabaea7846ae27aa14f4d
qianchaochen <qianchaochen@126.com>
2019-10-07 01:24:49 +0800

    189.旋转数组:给定一个数组，将数组中的元素向右移动 k 个位置

---
+++ src/com/qcc/Solution189.java
@@ -0,0 +1,55 @@
+package com.qcc;
+
+import java.util.Arrays;
+
+/**
+ * @Description 旋转数组
+ * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,2,3,4,5,6,7] 和 k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右旋转 1 步: [7,1,2,3,4,5,6]
+ * 向右旋转 2 步: [6,7,1,2,3,4,5]
+ * 向右旋转 3 步: [5,6,7,1,2,3,4]
+ * 示例 2:
+ * <p>
+ * 输入: [-1,-100,3,99] 和 k = 2
+ * 输出: [3,99,-1,-100]
+ * 解释:
+ * 向右旋转 1 步: [99,-1,-100,3]
+ * 向右旋转 2 步: [3,99,-1,-100]
+ * 说明:
+ * <p>
+ * 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+ * 要求使用空间复杂度为 O(1) 的 原地 算法。
+ * @Author：create by qianchaochen at 2019年10月06日18:19
+ */
+public class Solution189 {
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        rotate(nums, 3);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    /**
+     * 循环 k 次,每次移一位
+     *
+     * @param nums
+     * @param k
+     */
+    public static void rotate(int[] nums, int k) {
+        while (k > 0) {
+            int length = nums.length;
+            int temp = nums[length - 1];
+            for (int i = length - 1; i > 0; i--) {
+                nums[i] = nums[i - 1];
+            }
+            nums[0] = temp;
+            k--;
+        }
+    }
+}
