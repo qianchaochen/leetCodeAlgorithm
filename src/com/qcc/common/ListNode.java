f0f19c34a0f35e5dbe2cd12e274ba5e8cc26341a
qianchaochen <qianchaochen@126.com>
2019-10-09 09:50:42 +0800

    21.合并两个有序链表

---
+++ src/com/qcc/common/ListNode.java
@@ -0,0 +1,20 @@
+package com.qcc.common;
+
+/**
+ * @Description
+ * @Author qianchaochen
+ * @Date 2019年10月09日 8:59
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+//
+//    @Override
+//    public String toString() {
+//        return String.valueOf(this.val);
+//    }
+}
