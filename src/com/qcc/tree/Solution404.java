bbe5e2f47a757cb414ed26d8d02103e0b4d05887
qianchaochen <qianchaochen@126.com>
2019-10-24 10:06:00 +0800

    404. 左叶子之和

---
+++ src/com/qcc/tree/Solution404.java
@@ -0,0 +1,49 @@
+package com.qcc.tree;
+
+import com.qcc.common.TreeNode;
+import sun.reflect.generics.tree.Tree;
+
+/**
+ * @Description 404. 左叶子之和
+ * 计算给定二叉树的所有左叶子之和。
+ * <p>
+ * 示例：
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+ * @Author qianchaochen
+ * @Date 2019年10月24日 9:15
+ */
+public class Solution404 {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        TreeNode t1 = new TreeNode(9);
+        TreeNode t2 = new TreeNode(20);
+        TreeNode t3 = new TreeNode(15);
+        TreeNode t4 = new TreeNode(7);
+        root.left = t1;
+        root.right = t2;
+        t2.left = t3;
+        t2.right = t4;
+
+        Solution404 inst = new Solution404();
+        int res = inst.sumOfLeftLeaves(root);
+        System.out.println(res);
+
+    }
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        int sum = 0;
+        if (root == null) return 0;
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            sum += root.left.val;
+        }
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right) + sum;
+    }
+
+}
