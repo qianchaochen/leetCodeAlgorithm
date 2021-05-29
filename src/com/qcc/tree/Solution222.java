e234514ff3b18c41855158ced8ec94b61443ccb6
50240500 <qianchaochen@mindray.com>
2021-05-29 11:36:28 +0800

    222.完全二叉树节点个数

---
+++ src/com/qcc/tree/Solution222.java
@@ -0,0 +1,42 @@
+package com.qcc.tree;
+
+import com.qcc.common.TreeNode;
+
+/**
+ 222. 完全二叉树的节点个数
+ 给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。
+
+ 完全二叉树 的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第 h 层，则该层包含 1~ 2h 个节点。
+
+
+
+ 示例 1：
+
+
+ 输入：root = [1,2,3,4,5,6]
+ 输出：6
+ 示例 2：
+
+ 输入：root = []
+ 输出：0
+ 示例 3：
+
+ 输入：root = [1]
+ 输出：1
+ * created by qianchaochen at 2021/05/29 11:30
+ */
+public class Solution222 {
+    public int count = 0;
+    public int countNodes(TreeNode root) {
+        inOrder(root);
+        return count;
+    }
+
+    public void inOrder(TreeNode root){
+        if (root != null) {
+            inOrder(root.left);
+            count++;
+            inOrder(root.right);
+        }
+    }
+}
